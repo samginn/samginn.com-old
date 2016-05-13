6bd2aba5a0f073bacf75a626e54a99237607a668
Sam Ginn <sam@samginn.com>
2016-05-12 23:40:05 -0700

    Testing deployment

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@
 
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files src/ ${INSTANCE_NAME}:/var/www/samginn.com/public
+ssh-keygen -q -N "" -f ~/.ssh/google_compute_engine
+sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files /home/ubuntu/samginn.com/src/ ${INSTANCE_NAME}:/var/www/samginn.com/public
