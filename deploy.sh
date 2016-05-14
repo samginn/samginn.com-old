82a5a36c5ced4682b7f58a898e37e9db920330aa
Sam Ginn <sam@samginn.com>
2016-05-13 23:37:07 -0700

    Fixing Circle-CI deployment

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 set -e
 
 ssh-keygen -q -N "" -f ~/.ssh/google_compute_engine
-sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files /home/ubuntu/samginn.com/src/ ${INSTANCE_NAME}:/var/www/samginn.com/public
+sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files src/* circleci@hydrogen:/var/www/samginn.com/public
