64b094f6b3d1497f7024e75a2bd3b6ca6cf2d960
Sam Ginn <sam@samginn.com>
2016-05-13 23:39:56 -0700

    Publishing for deployment

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ssh-keygen -q -N "" -f ~/.ssh/google_compute_engine
-sudo /opt/google-cloud-sdk/bin/gcloud compute copy-files src/* circleci@hydrogen:/var/www/samginn.com/public
